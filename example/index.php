b02b130b21aedd28aa8a8fb5c0f0cf59564dd416
davidecesarano <davide.cesarano@unipegaso.it>
2018-12-28 12:18:50 +0100

    Update example
    Signed-off-by: davidecesarano <davide.cesarano@unipegaso.it>

---
+++ example/index.php
@@ -0,0 +1,49 @@
+<?php 
+
+    require __DIR__ . '/../vendor/autoload.php';
+        
+    use Embryo\Http\Emitter\Emitter;
+    use Embryo\Http\Server\MiddlewareDispatcher;
+    use Embryo\Http\Factory\{ServerRequestFactory, ResponseFactory};
+    use Embryo\Session\Session;
+    use Embryo\Session\Middleware\SessionMiddleware;
+    use Embryo\Translate\Translate;
+    use Embryo\Translate\Middleware\SetLocaleMiddleware;
+    use Psr\Http\Message\ResponseInterface;
+    use Psr\Http\Message\ServerRequestInterface;
+    use Psr\Http\Server\MiddlewareInterface;
+    use Psr\Http\Server\RequestHandlerInterface;
+    
+    $request    = (new ServerRequestFactory)->createServerRequestFromServer();
+    $response   = (new ResponseFactory)->createResponse(200);
+    $middleware = new MiddlewareDispatcher;
+    $session    = new Session;
+    
+    class TestTranslateMiddleware implements MiddlewareInterface
+    {
+        public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+        {
+            $translate = new Translate(__DIR__.DIRECTORY_SEPARATOR.'lang');
+            $translate->setMessages();
+            $session   = $request->getAttribute('session');
+            $messages  = $translate->getMessages($session->get('language'));
+            $response  = $handler->handle($request);
+            return $response->write('<p>'.$messages->get('hello').', '.$messages->get('name', ['name' => 'David']));
+        }
+    }
+
+    $middleware->add(
+        (new SessionMiddleware)
+            ->setSession($session)
+            ->setOptions([
+                'use_cookies'      => false,
+                'use_only_cookies' => true
+            ])
+    );
+
+    $middleware->add(SetLocaleMiddleware::class);
+    $middleware->add(TestTranslateMiddleware::class);
+    $response = $middleware->dispatch($request, $response);
+    
+    $emitter = new Emitter;
+    $emitter->emit($response);
---
+++ example/lang/en/messages.php
@@ -0,0 +1,6 @@
+<?php 
+
+    return [
+        'hello' => 'Hello',
+        'name' => 'My name is {name}'
+    ];
